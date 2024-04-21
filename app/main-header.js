c8da271bb35bc306a9ce21e9097173cbfcaab50a
raj779401 <raj779401@gmail.com>
2024-04-21 23:28:21 +0530

    using default <Image> component

--- app/main-header.js
+++ app/main-header.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 export default function MainHeader(){
     return <header className={classes.header}>
         <Link  href="/" className= {classes.logo}>
-        <img src={logoImg.src} alt='A plate with food in it'/>
+        <Image src={logoImg} alt='A plate with food in it' priority/>
         Next Level Food
         </Link> 
 
